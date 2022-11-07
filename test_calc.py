a77336da139115987b246a6c4c43248e64cdf49a
Mohammed <rexmecse@gmail.com>
2022-11-06 18:09:06 -0700

    First commit

---
+++ test_calc.py
@@ -0,0 +1,16 @@
+def add(x, y):
+    return x + y
+
+
+def subtract(x, y):
+    return x - y
+
+
+def test_add():
+    result = add(1, 2)
+    assert result == 3
+
+
+def test_subtract():
+    result = subtract(2, 1)
+    assert result == 1
